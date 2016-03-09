b4049ede09d5cac41008f173403dcfb5b937b00c
moshu1234 <187032677@qq.com>
2016-03-09 14:24:20 +0800

    merge sliding menu codes
    # Conflicts:
    #	FatBaby/.idea/modules.xml
    
    #	FatBaby/SlidingMenu/library/build/intermediates/incremental/mergeReleaseAssets/merger.xml
    
    #	FatBaby/SlidingMenu/library/build/intermediates/incremental/mergeReleaseJniLibFolders/merger.xml
    
    #	FatBaby/SlidingMenu/library/build/intermediates/incremental/packageReleaseResources/merger.xml
    
    #	FatBaby/SlidingMenu/library/build/outputs/aar/SlidingMenu/library-release.aar
    
    #	FatBaby/SlidingMenu/library/src/com/jeremyfeinstein/slidingmenu/lib/CustomViewAbove.java

---
+++ FatBaby/app/src/main/java/com/example/andrewliu/fatbaby/SlidMenu/MainTab02.java
@@ -0,0 +1,20 @@
+package com.example.andrewliu.fatbaby.SlidMenu;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.andrewliu.fatbaby.R;
+
+public class MainTab02 extends Fragment
+{
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		View messageLayout = inflater.inflate(R.layout.main_tab_02, container, false);
+		return messageLayout;
+	}
+
+}
--- FatBaby/app/src/main/java/com/example/andrewliu/fatbaby/infoShow.java
+++ FatBaby/app/src/main/java/com/example/andrewliu/fatbaby/infoShow.java
@@ -1,27 +1,44 @@
 package com.example.andrewliu.fatbaby;
 
-import android.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.widget.ProgressBar;
+import android.view.View;
 
 import com.example.andrewliu.fatbaby.BodyCirleShow.BodyProgress;
+import com.example.andrewliu.fatbaby.SlidMenu.MainTab01;
+import com.example.andrewliu.fatbaby.SlidMenu.MainTab02;
+import com.example.andrewliu.fatbaby.SlidMenu.MainTab03;
+import com.example.andrewliu.fatbaby.SlidMenu.MenuLeftFragment;
+import com.example.andrewliu.fatbaby.SlidMenu.MenuRightFragment;
 import com.example.andrewliu.fatbaby.progressbar.CircleProgressBar;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class infoShow extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class infoShow extends SlidingFragmentActivity {
     CircleProgressBar pb1,pb2,pb3,pb4,pb5;
     BodyProgress mBodyProgress;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowHomeEnabled(true);
-        actionBar.setLogo(R.drawable.left);
-        actionBar.setDisplayUseLogoEnabled(true);
+
+    private ViewPager mViewPager;
+    private FragmentPagerAdapter mAdapter;
+    private List<Fragment> mFragments = new ArrayList<Fragment>();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_show);
+
+        // 初始化SlideMenu
+        initRightMenu();
+        // 初始化ViewPager
+        initViewPager();
 
         pb1=(CircleProgressBar)findViewById(R.id.roundProgressBar1);
         setProgressBarProgress(pb1,30,"day 1");
@@ -31,11 +48,12 @@ public class infoShow extends AppCompatActivity {
         setProgressBarProgress(pb3,50,"day 3");
         pb4=(CircleProgressBar)findViewById(R.id.roundProgressBar4);
         setProgressBarProgress(pb4,60,"day 4");
-        pb5=(CircleProgressBar)findViewById(R.id.roundProgressBar5);
+        pb5 = (CircleProgressBar)findViewById(R.id.roundProgressBar5);
         setProgressBarProgress(pb5,70,"day 5");
 
         mBodyProgress=(BodyProgress)findViewById(R.id.bodyProgress);
         setBodyProgress(mBodyProgress,60,"加油哦");
+
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -47,5 +65,70 @@ public class infoShow extends AppCompatActivity {
     }
     public void setBodyProgress(BodyProgress bp, int progress, String text){
         bp.updateProgressText(progress,text);
+    }
+    private void initViewPager()
+    {
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+        MainTab01 tab01 = new MainTab01();
+        MainTab02 tab02 = new MainTab02();
+        MainTab03 tab03 = new MainTab03();
+        mFragments.add(tab01);
+        mFragments.add(tab02);
+        mFragments.add(tab03);
+        /**
+         * 初始化Adapter
+         */
+        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager())
+        {
+            @Override
+            public int getCount()
+            {
+                return mFragments.size();
+            }
+
+            @Override
+            public Fragment getItem(int arg0)
+            {
+                return mFragments.get(arg0);
+            }
+        };
+        mViewPager.setAdapter(mAdapter);
+    }
+
+    private void initRightMenu()
+    {
+
+        Fragment leftMenuFragment = new MenuLeftFragment();
+        setBehindContentView(R.layout.left_menu_frame);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.id_left_menu_frame, leftMenuFragment).commit();
+        SlidingMenu menu = getSlidingMenu();
+        menu.setMode(SlidingMenu.LEFT_RIGHT);
+        // 设置触摸屏幕的模式
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        // 设置滑动菜单视图的宽度
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+//		menu.setBehindWidth()
+        // 设置渐入渐出效果的值
+        menu.setFadeDegree(0.35f);
+        // menu.setBehindScrollScale(1.0f);
+        menu.setSecondaryShadowDrawable(R.drawable.shadow);
+        //设置右边（二级）侧滑菜单
+        menu.setSecondaryMenu(R.layout.right_menu_frame);
+        Fragment rightMenuFragment = new MenuRightFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.id_right_menu_frame, rightMenuFragment).commit();
+    }
+
+    public void showLeftMenu(View view)
+    {
+        getSlidingMenu().showMenu();
+    }
+
+    public void showRightMenu(View view)
+    {
+        getSlidingMenu().showSecondaryMenu();
     }
 }
